f9a553299833417fc5277d51f9c06165250981bd
J <jacksonhick330@gmail.com>
2025-11-27 10:07:25 +0000

    Update package dependencies and add deployment scripts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MuseMap-Website/', // Replace with your repo name
   server: {
     port: 3000,
     strictPort: true, // Fail if port is in use, to avoid jumping to unwanted ports
